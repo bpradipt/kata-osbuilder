67ede61361f86ec1bbf697041a458dd05d43ef66
Cole Robinson <crobinso@redhat.com>
2020-03-04 15:33:12 -0500

    osbuilder.sh: Improve coding style
    * Split blocks into functions
    * Mark local and readonly variables
    * Should be no functional change
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- fedora-kata-osbuilder.sh
+++ fedora-kata-osbuilder.sh
@@ -3,98 +3,145 @@
 # This script builds the kata appliance initrd and image. It is invoked
 # at RPM install %post time and via kata-osbuilder-generate.service
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
+
+[ -n "${DEBUG:-}" ] && set -o xtrace
+
+readonly IMAGE_TOPDIR="/var/cache/kata-containers"
+readonly KERNEL_SYMLINK="${IMAGE_TOPDIR}/vmlinuz.container"
+readonly KVERSION=`uname -r`
+readonly SCRIPTNAME="$0"
+
+readonly DRACUT_OVERLAY=`mktemp --directory -t kata-dracut-overlay-XXXXXX`
+readonly DRACUT_ROOTFS=`mktemp --directory -t kata-dracut-rootfs-XXXXXX`
+readonly DRACUT_IMAGES=`mktemp --directory -t kata-dracut-images-XXXXXX`
+trap exit_handler EXIT
+
+readonly GENERATED_IMAGE="${DRACUT_IMAGES}/kata-containers.img"
+readonly GENERATED_INITRD="${DRACUT_IMAGES}/kata-containers-initrd.img"
+
+
+KERNEL_PATH=""
+COMMAND=""
+
 
 die()
 {
-    echo "ERROR: $*" >&2
+    echo "ERROR: ${SCRIPTNAME}: $*" >&2
     exit 1
 }
 
-[ "$(id -u)" -eq 0 ] || die "$0: must be run as root"
 
-COMMAND="$1"
-[ -z "$COMMAND" ] && COMMAND="check"
-[ "$COMMAND" != "check" -a "$COMMAND" != "regenerate" ] && die "$0: Unknown command=$COMMAND"
+info()
+{
+    echo "${SCRIPTNAME}: $*"
+}
 
-IMAGE_TOPDIR="/var/cache/kata-containers"
-KERNEL_SYMLINK="${IMAGE_TOPDIR}/vmlinuz.container"
 
-KVERSION=`uname -r`
-KERNEL_PATH=""
-for VMNAME in vmlinuz vmlinux; do
-    TRYPATH="/lib/modules/$KVERSION/$VMNAME"
-    if [ -e "$TRYPATH" ] ; then
-        KERNEL_PATH="$TRYPATH"
-        break
+exit_handler()
+{
+    rm -rf "${DRACUT_OVERLAY}" "${DRACUT_ROOTFS}" "${DRACUT_IMAGES}"
+}
+
+
+parse_args()
+{
+    COMMAND="${1:-}"
+    [ -z "$COMMAND" ] && COMMAND="check"
+    [ "$COMMAND" != "check" ] && [ "$COMMAND" != "regenerate" ] && die "Unknown command=$COMMAND"
+    return 0
+}
+
+
+find_host_kernel_path()
+{
+    local vmname
+    for vmname in vmlinuz vmlinux; do
+        local trypath="/lib/modules/$KVERSION/$vmname"
+        if [ -e "$trypath" ] ; then
+            KERNEL_PATH="$trypath"
+            break
+        fi
+    done
+
+    [ -z "$KERNEL_PATH" ] && die "Didn't find kernel path for version=$KVERSION"
+
+    if [ "regenerate" != "$COMMAND" ]; then
+        local linked_kernel=$(readlink -n "${KERNEL_SYMLINK}" || :)
+        if [ "${KERNEL_PATH}" = "${linked_kernel}" ] ; then
+            info "symlink=${KERNEL_SYMLINK} already points to host kernel=${KERNEL_PATH}"
+            info "Nothing to generate. Exiting."
+            exit 0
+        fi
     fi
-done
-
-[ -z "$KERNEL_PATH" ] && die "$0: Didn't find kernel path for version=$KVERSION"
-
-if [ "regenerate" != "$COMMAND" ]; then
-    LINKED_KERNEL=$(readlink -n "${KERNEL_SYMLINK}" || :)
-    if [ "${KERNEL_PATH}" = "${LINKED_KERNEL}" ] ; then
-        echo "$0: symlink=${KERNEL_SYMLINK} already points to host kernel=${KERNEL_PATH}"
-        echo "$0: Nothing to generate. Exiting."
-        exit 0
-    fi
-fi
+}
 
 
-cd /usr/libexec/kata-containers/osbuilder
+move_images()
+{
+    # Move images into place
+    local image_osbuilder_dir="${IMAGE_TOPDIR}/osbuilder-images"
+    local image_dir="${image_osbuilder_dir}/$KVERSION"
+    local initrd_dest_path="${image_dir}/fedora-kata-${KVERSION}.initrd"
+    local image_dest_path="${image_dir}/fedora-kata-${KVERSION}.img"
 
-DRACUT_OVERLAY=`mktemp --directory -t kata-dracut-overlay-XXXXXX`
-DRACUT_ROOTFS=`mktemp --directory -t kata-dracut-rootfs-XXXXXX`
-DRACUT_IMAGES=`mktemp --directory -t kata-dracut-images-XXXXXX`
-trap "{ rm -rf ${DRACUT_OVERLAY} ${DRACUT_ROOTFS} ${DRACUT_IMAGES}; }" EXIT
+    # This blows away the entire osbuilder-images/ dir, deleting any
+    # previously cached content
+    rm -rf "${image_osbuilder_dir}"
+    mkdir -p "${image_dir}"
 
+    ln -sf ${KERNEL_PATH} ${KERNEL_SYMLINK}
 
-export AGENT_SOURCE_BIN="/usr/libexec/kata-containers/osbuilder/agent/kata-agent"
-TARGET_IMAGE="${DRACUT_IMAGES}/kata-containers.img" \
-TARGET_INITRD="${DRACUT_IMAGES}/kata-containers-initrd.img" \
-OSBUILDER_VERSION="fedora-osbuilder-version-unknown"
-DRACUT_CONF_DIR="./dracut/dracut.conf.d"
-DRACUT_KMODULES=`source ${DRACUT_CONF_DIR}/10-drivers.conf; echo "$drivers"`
-DRACUT_OPTIONS="--no-compress --conf /dev/null --confdir ./dracut/dracut.conf.d"
+    mv ${GENERATED_INITRD} ${initrd_dest_path}
+    ln -sf ${initrd_dest_path} ${IMAGE_TOPDIR}/kata-containers-initrd.img
 
-# Build the dracut overlay fs
-./rootfs-builder/rootfs.sh -o ${OSBUILDER_VERSION} -r ${DRACUT_OVERLAY}
-mkdir -p ${DRACUT_OVERLAY}/etc/modules-load.d
-echo ${DRACUT_KMODULES} | tr " " "\n" > ${DRACUT_OVERLAY}/etc/modules-load.d/kata-modules.conf
-
-# Build the initrd
-dracut ${DRACUT_OPTIONS} \
-    --include ${DRACUT_OVERLAY} \
-    / ${TARGET_INITRD} ${KVERSION}
-
-# Extract initrd filesystem for image build
-cat ${TARGET_INITRD} | \
-    cpio --extract --preserve-modification-time --make-directories --directory=${DRACUT_ROOTFS}
-
-# Build the FS image
-./image-builder/image_builder.sh -o ${TARGET_IMAGE} ${DRACUT_ROOTFS}
-
-# This is a workaround till issue[0] is fixed, released and packaged.
-# [0]: https://github.com/kata-containers/osbuilder/issues/394
-rm image-builder/nsdax
+    mv ${GENERATED_IMAGE} ${image_dest_path}
+    ln -sf ${image_dest_path} ${IMAGE_TOPDIR}/kata-containers.img
+}
 
 
-# Move images into place
-IMAGE_OSBUILDER_DIR="${IMAGE_TOPDIR}/osbuilder-images"
-IMAGE_DIR="${IMAGE_OSBUILDER_DIR}/$KVERSION"
-INITRD_NAME="${IMAGE_DIR}/fedora-kata-${KVERSION}.initrd"
-IMAGE_NAME="${IMAGE_DIR}/fedora-kata-${KVERSION}.img"
+main()
+{
+    [ "$(id -u)" -eq 0 ] || die "$0: must be run as root"
 
-# This blows away the entire osbuilder-images/ dir, deleting any
-# previously cached content
-rm -rf "${IMAGE_OSBUILDER_DIR}"
-mkdir -p "${IMAGE_DIR}"
+    parse_args $*
+    find_host_kernel_path
 
-ln -sf ${KERNEL_PATH} ${KERNEL_SYMLINK}
+    cd /usr/libexec/kata-containers/osbuilder
 
-mv ${TARGET_INITRD} ${INITRD_NAME}
-ln -sf ${INITRD_NAME} ${IMAGE_TOPDIR}/kata-containers-initrd.img
+    export AGENT_SOURCE_BIN="/usr/libexec/kata-containers/osbuilder/agent/kata-agent"
+    local osbuilder_version="fedora-osbuilder-version-unknown"
+    local dracut_conf_dir="./dracut/dracut.conf.d"
+    local dracut_kmodules=`source ${dracut_conf_dir}/10-drivers.conf; echo "$drivers"`
 
-mv ${TARGET_IMAGE} ${IMAGE_NAME}
-ln -sf ${IMAGE_NAME} ${IMAGE_TOPDIR}/kata-containers.img
+    # Build the dracut overlay fs
+    ./rootfs-builder/rootfs.sh -o ${osbuilder_version} -r ${DRACUT_OVERLAY}
+    mkdir -p ${DRACUT_OVERLAY}/etc/modules-load.d
+    echo ${dracut_kmodules} | tr " " "\n" > ${DRACUT_OVERLAY}/etc/modules-load.d/kata-modules.conf
+
+    # Build the initrd
+    dracut  \
+        --no-compress \
+        --conf /dev/null \
+        --confdir ${dracut_conf_dir} \
+        --include ${DRACUT_OVERLAY} \
+        / ${GENERATED_INITRD} ${KVERSION}
+
+    # Extract initrd filesystem for image build
+    cat ${GENERATED_INITRD} | \
+        cpio --extract --preserve-modification-time --make-directories --directory=${DRACUT_ROOTFS}
+
+    # Build the FS image
+    ./image-builder/image_builder.sh -o ${GENERATED_IMAGE} ${DRACUT_ROOTFS}
+
+    # This is a workaround till issue[0] is fixed, released and packaged.
+    # [0]: https://github.com/kata-containers/osbuilder/issues/394
+    rm image-builder/nsdax
+
+    move_images
+}
+
+
+main $*
